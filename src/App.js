671e5535ec5f8cfd5057c20d0ef0fefab58bac39
Bekhzod Abdullaev <b.abdullaev@dotsource.de>
2022-08-03 14:49:29 +0200

    refacotred junk files. added qr code rendering, applied styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="qr-container">
+      <div className="qr-wrapper">
+        <div className="qr-wrapper__image">
+          <img src="./images/image-qr-code.png" alt="qr-code" />
+        </div>
+        <div className="qr-wrapper__text">
+          <h3 className="qr-wrapper__text-headline">
+            Improve your front-end skills by building projects
+          </h3>
+          <p className="qr-wrapper__text-introText">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
